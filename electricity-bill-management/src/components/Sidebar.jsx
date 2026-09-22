ecd12a1b4a1b8de9198db281d265ac3d09fa07ea
Abin VK <vettikkadanabin@gmail.com>
2026-09-22 21:59:32 +0530

    bill project

---
+++ electricity-bill-management/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+function Sidebar() {
+  return (
+    <aside>
+      <h2>PowerCollect</h2>
+
+      <nav>
+        <a href="#">Dashboard</a>
+        <a href="#">Bills</a>
+        <a href="#">Collectors</a>
+        <a href="#">Reports</a>
+        <a href="#">Settings</a>
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
