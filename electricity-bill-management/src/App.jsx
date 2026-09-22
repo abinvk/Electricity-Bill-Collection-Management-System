3e2d7fdbe54c412da80450a5ab336b12e2cbf5b2
Abin VK <vettikkadanabin@gmail.com>
2026-09-22 22:23:49 +0530

    bill-project

--- electricity-bill-management/src/App.jsx
+++ electricity-bill-management/src/App.jsx
@@ -1,4 +1,5 @@
 import Sidebar from "./components/Sidebar";
+import StatCard from "./components/StatCard";
 
 function App() {
   return (
@@ -8,6 +9,13 @@ function App() {
       <main>
         <h1>Dashboard</h1>
         <p>Electricity Bill Collection Management</p>
+
+        <div className="stats">
+          <StatCard title="Total Bills" value="12,540" />
+          <StatCard title="Collected" value="10,820" />
+          <StatCard title="Pending Bills" value="1,720" />
+          <StatCard title="Collectors" value="24" />
+        </div>
       </main>
     </div>
   );
